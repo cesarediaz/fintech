b701215a26874880d3575ac40cd3eeac31bea778
Cesar Diaz <cesare.diaz@gmail.com>
2016-09-12 20:47:02 -0300

    modified existent and added new test

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -13,4 +13,18 @@ RSpec.feature 'user use login', type: :feature do
 
     expect(page).to have_text("Welcome #{user.email}")
   end
+
+  scenario 'failure', js: true do
+    user = create(:user)
+
+    visit(new_user_session_path)
+
+    fill_in 'user[email]', with: 'whatever@email.com'
+    fill_in 'user[password]', with: 'failpassword'
+
+    click_button 'Log in'
+
+    expect(page).not_to have_text('Sign out')
+    expect(page).to have_text('Please sign in')
+  end
 end
