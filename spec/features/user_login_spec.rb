8518b9e2830b89fbb15c8df5d8f6cda33c05e8c5
Cesar Diaz <cesare.diaz@gmail.com>
2016-09-12 20:18:24 -0300

    fixed sucessful login test

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature 'user use login', type: :feature do
-  before(:each) do
-    login_as create(:user)
-  end
+  scenario 'sucessful', js: true do
+    user = create(:user)
 
-  scenario 'index', js: true do
-    user = User.last
-    visit(root_path)
+    visit(new_user_session_path)
+
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+
+    click_button 'Log in'
 
     expect(page).to have_text("Welcome #{user.email}")
   end
