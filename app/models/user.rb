4d0ebf947a8f6c6da43cb79c1d1849987e96c121
Cesar Diaz <cesare.diaz@gmail.com>
2016-09-12 21:55:47 -0300

    added aditional validation for weak password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validate :strong_password
+
+  private
+
+  def weak_words
+    %w(password mypassword)
+  end
+
+  def strong_password
+    errors.add(:password, 'too weak') if weak_words.include?(password)
+  end
 end
